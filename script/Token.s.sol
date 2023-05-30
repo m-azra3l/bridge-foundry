be066088b3270befd21528d754d3fa8a4ff61ab8
Michael <gadreelazazel@gmail.com>
2023-05-30 11:46:16 +0100

    Update and Create

---
+++ script/Token.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+import "forge-std/Script.sol";
+import "../src/Token.sol";
+
+contract DeployToken is Script {
+  function run() external {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(deployerPrivateKey);
+    Token token = new Token();
+    vm.stopBroadcast();
+  }
+}
